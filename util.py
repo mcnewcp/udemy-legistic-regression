364d64dbdf6666f0e2349fbffc69184af2b0bb8d
Coy McNew <mcnewcp@gmail.com>
2022-01-16 12:28:05 -0600

    adjust getData for my filepath

---
+++ util.py
@@ -0,0 +1,34 @@
+import numpy as np
+import pandas as pd
+
+data_path = '../data/fer2013.csv'
+
+def getData(balance_ones=True, Ntest=1000):
+    # images are 48x48 = 2304 size vectors
+    Y = []
+    X = []
+    first = True
+    for line in open(data_path):
+        if first:
+            first = False
+        else:
+            row = line.split(',')
+            Y.append(int(row[0]))
+            X.append([int(p) for p in row[1].split()])
+
+    X, Y = np.array(X) / 255.0, np.array(Y)
+
+    # shuffle and split
+    X, Y = shuffle(X, Y)
+    Xtrain, Ytrain = X[:-Ntest], Y[:-Ntest]
+    Xvalid, Yvalid = X[-Ntest:], Y[-Ntest:]
+
+    if balance_ones:
+        # balance the 1 class
+        X0, Y0 = Xtrain[Ytrain!=1, :], Ytrain[Ytrain!=1]
+        X1 = Xtrain[Ytrain==1, :]
+        X1 = np.repeat(X1, 9, axis=0)
+        Xtrain = np.vstack([X0, X1])
+        Ytrain = np.concatenate((Y0, [1]*len(X1)))
+
+    return Xtrain, Ytrain, Xvalid, Yvalid
