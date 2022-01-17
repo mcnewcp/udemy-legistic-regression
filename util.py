1c0a60d39664cd579a751493b87789152e7b365b
Coy McNew <mcnewcp@gmail.com>
2022-01-17 12:35:41 -0600

    added various utility functions for NN and log reg

--- util.py
+++ util.py
@@ -26,6 +26,7 @@ def getData(balance_ones=True, Ntest=1000):
 
     if balance_ones:
         # balance the 1 class
+        # since class 1 is imbalanced, we're repeating it 9 times here
         X0, Y0 = Xtrain[Ytrain!=1, :], Ytrain[Ytrain!=1]
         X1 = Xtrain[Ytrain==1, :]
         X1 = np.repeat(X1, 9, axis=0)
@@ -33,3 +34,99 @@ def getData(balance_ones=True, Ntest=1000):
         Ytrain = np.concatenate((Y0, [1]*len(X1)))
 
     return Xtrain, Ytrain, Xvalid, Yvalid
+
+#initialize weights and bias terms before model training
+#M1 - input size, M2 - output size
+def init_weight_and_bias(M1, M2):
+    W = np.random.randn(M1, M2) / np.sqrt(M1)
+    b = np.zeros(M2)
+    #converting to float32 so they can be used in tenserflow
+    return W.astype(np.float32), b.astype(np.float32)
+
+#used for CNN
+#shape - tuple of 4 vals
+def init_filter(shape, poolsz):
+    w = np.random.randn(*shape) * np.sqrt(2) / np.sqrt(np.prod(shape[1:]) + shape[0]*np.prod(shape[2:] / np.prod(poolsz)))
+    return w.astype(np.float32)
+
+#rectifier linear unit used for activation function 
+def relu(x):
+    return x * (x > 0)
+
+def sigmoid(A):
+    return 1 / (1 + np.exp(-A))
+
+
+def softmax(A):
+    expA = np.exp(A)
+    return expA / expA.sum(axis=1, keepdims=True)
+
+#calculates cross-entropy from def sigmoid
+def sigmoid_cost(T, Y):
+    return -(T*np.log(Y) + (1-T)*np.log(1-Y)).sum()
+
+#more general cross-entropy, works for softmax
+def cost(T, Y):
+    return -(T*np.log(Y)).sum()
+
+#less computationly intensive version of cost()
+def cost2(T, Y):
+    # same as cost(), just uses the targets to index Y
+    # instead of multiplying by a large indicator matrix with mostly 0s
+    N = len(T)
+    return -np.log(Y[np.arange(N), T]).mean()
+
+def error_rate(targets, predictions):
+    return np.mean(targets != predictions)
+
+#turns class target vector Nx1 into an indicator matrix N x K with labels 0 or 1
+def y2indicator(y):
+    N = len(y)
+    K = len(set(y))
+    ind = np.zeros((N, K))
+    for i in range(N):
+        ind[i, y[i]] = 1
+    return ind
+
+def getImageData():
+    Xtrain, Ytrain, Xvalid, Yvalid = getData()
+    N, D = Xtrain.shape
+    d = int(np.sqrt(D))
+    Xtrain = Xtrain.reshape(-1, 1, d, d)
+    Xvalid = Xvalid.reshape(-1, 1, d, d)
+    return Xtrain, Ytrain, Xvalid, Yvalid
+
+
+#this does almost the same thing as getData except it only loads data with class 0 or 1
+def getBinaryData():
+    Y = []
+    X = []
+    first = True
+    for line in open('fer2013.csv'):
+        if first:
+            first = False
+        else:
+            row = line.split(',')
+            y = int(row[0])
+            if y == 0 or y == 1:
+                Y.append(y)
+                X.append([int(p) for p in row[1].split()])
+    return np.array(X) / 255.0, np.array(Y)
+
+
+def crossValidation(model, X, Y, K=5):
+    # split data into K parts
+    X, Y = shuffle(X, Y)
+    sz = len(Y) // K
+    errors = []
+    for k in range(K):
+        xtr = np.concatenate([ X[:k*sz, :], X[(k*sz + sz):, :] ])
+        ytr = np.concatenate([ Y[:k*sz], Y[(k*sz + sz):] ])
+        xte = X[k*sz:(k*sz + sz), :]
+        yte = Y[k*sz:(k*sz + sz)]
+
+        model.fit(xtr, ytr)
+        err = model.score(xte, yte)
+        errors.append(err)
+    print("errors:", errors)
+    return np.mean(errors)
